4ab516e5c3befcd050e03db3dc52c0571ca83b45
Ahmed-Ri <121891937+Ahmed-Ri@users.noreply.github.com>
2023-07-09 18:36:28 +0200

    barre de navigation

--- connexion.php
+++ connexion.php
@@ -1,19 +1,29 @@
 <?php
 include('db.php');
+
+$erreurs = [
+    'emailerreur' => '',
+    'mdperreur' => '',
+];
 if (isset($_POST['email']) && isset($_POST['mdp'])) {
     $email = $_POST['email'];
     $mdp = $_POST['mdp'];
 
     $sql = "SELECT * FROM utilisateur WHERE email='$email' AND mdp='$mdp'";
-
-    $req = mysqli_query($conn, $sql);
-
-    $num_ligne = mysqli_num_rows($req);
-
-    if ($num_ligne > 0) {
-        header("Location:bienvenue.php");
+    if (empty($email)) {
+        $erreurs['emailerreur'] = 'Entrez votre Email!';
+    } else if (empty($mdp)) {
+        $erreurs['mdperreur'] = 'Entrez votre mot de passe';
     } else {
-        echo "Adresse mail ou mot de passe incorectes";
+        $req = mysqli_query($conn, $sql);
+
+        $num_ligne = mysqli_num_rows($req);
+
+        if ($num_ligne > 0) {
+            header("Location:bienvenue.php");
+        } else {
+            $erreurs['mdperreur'] = 'Mot de passe incorrect';
+        }
     }
 }
 
@@ -37,8 +47,10 @@ if (isset($_POST['email']) && isset($_POST['mdp'])) {
         <form action="" method="POST">
             <label>Adresse Mail</label>
             <input type="email" name="email" />
+            <div class="error"> <?php echo $erreurs['emailerreur']; ?> </div>
             <label for="">Mot de Passe</label>
             <input type="password" name="mdp" />
+            <div class="error"><?php echo $erreurs['mdperreur']; ?></div>
             <input type="submit" name="submit" value="valider" />
         </form>
         <p>S'inscrire <a href="inscription.php">Cliquez ici</a></p>
--- inscription.php
+++ inscription.php
@@ -3,6 +3,13 @@
 
 include("db.php");
 
+$errors = [
+    'nomError' => '',
+    'prenomError' => '',
+    'emailError' => '',
+    'mdpError' => '',
+];
+
 
 if (isset($_POST['submit'])) {
     $nom = $_POST['nom'];
@@ -11,9 +18,26 @@ if (isset($_POST['submit'])) {
     $mdp = $_POST['mdp'];
 
     $sql = "INSERT INTO utilisateur(Nom,Prenom,email,mdp) VALUE ('$nom','$prenom','$email','$mdp') ";
-    mysqli_query($conn, $sql);
-    header('Location:connexion.php');
+
+    if (empty($nom)) {
+        $errors['nomError'] = 'Entrez votre nom!';
+    } else if (empty($prenom)) {
+        $errors['prenomError'] = 'Entrez votre prénom!';
+    } else if (empty($email)) {
+        $errors['emailError'] = 'Entrez votre e_mail!';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['emailError'] = 'Entrez votre e_mail!';
+    } else if (empty($mdp)) {
+        $errors['mdpError'] = 'Entrez votre mot de passe!';
+    } else {
+        if (mysqli_query($conn, $sql)) {
+            header('Location:connexion.php');
+        } else {
+            echo 'ERREUR: ' . mysqli_error($conn);
+        }
+    }
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -29,15 +53,19 @@ if (isset($_POST['submit'])) {
 <body>
     <section>
         <h1>inscription</h1>
-        <form action="" method="POST">
+        <form action="inscription.php" method="POST">
             <label for="">nom</label>
             <input type="text" name="nom" />
+            <div class="error"><?php echo $errors['nomError'] ?></div>
             <label for="">prénom</label>
             <input type="text" name="prenom" />
+            <div class="error"><?php echo $errors['prenomError'] ?></div>
             <label for="email">Adresse Mail</label>
             <input type="email" name="email" />
+            <div class="error"><?php echo $errors['emailError'] ?></div>
             <label for="">password</label>
             <input type="password" name="mdp" />
+            <div class="error"><?php echo $errors['mdpError'] ?></div>
             <input type="submit" name="submit" value="valider" />
         </form>
         <p>Vous avez déja un compte? <a href="connexion.php">Cliquez ici</a> </p>
